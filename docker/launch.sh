1cbe481189dffd41a1cc39f4e0110b75be80b296
Shantanu Goel <shantanu.goel@gmail.com>
2019-04-27 01:32:12 +0530

    Don't remove container everytime by default

--- docker/launch.sh
+++ docker/launch.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 GID=`id -g`
-docker run --rm -it --user "${UID}":"${GID}" -p 8888:8888 -v "${PWD}"/../app:/home/user/app --name=dash-example shantanugoel/dash-example-docker $@
-#docker run --rm -it -p 8888:8888 -v "${pwd}"/../app:/home/user/app --name=dash-example shantanugoel/dash-example-docker $@
+docker run -it --user "${UID}":"${GID}" -p 8888:8888 -v "${PWD}"/../app:/home/user/app --name=dash-example shantanugoel/dash-example-docker $@
